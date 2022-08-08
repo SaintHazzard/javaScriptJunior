75e51f5718ce9cc34ba8068060eaf2212980ba41
SaintHazzard <chamitos1998@hotmail.com>
2022-08-08 13:48:55 -0500

    probando redeclaracion de variables

--- importDynamic/file.js
+++ importDynamic/file.js
@@ -1,3 +1,14 @@
 export function hello() {
  console.log(`Hello World exportado desde file.js y llamado con index.js al index.html`);
 }
+
+
+// Declarando variable global
+let variable = "No me puedes reasignar"
+
+// Reasignando variable global
+variable = variable + ", lero lero" // Esto puede hacerse sin ningún problema
+
+// Tratando de redeclarar variable
+let variable = "Claro que sí puedo"
+console.log(variable)
