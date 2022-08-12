1732008282be7f0416b8a7064272421d113cf298
SaintHazzard <chamitos1998@hotmail.com>
2022-08-12 18:09:16 -0500

    Script seleccion

--- codigo.js
+++ codigo.js
@@ -1,1 +1,20 @@
-import {  } from "../";
+function addValue() {
+ let inputCheck = document.querySelectorAll(`.select`);
+ inputCheck.forEach(Element => {
+  let addValue =Element.getAttribute(`id`)
+  Element.setAttribute(`value`, addValue );
+ })
+ // let idAdd=inputCheck.getAttribute(`id`);
+ // inputCheck.setAttribute(`value`, idAdd );
+ console.log(inputCheck);
+}
+
+// function selectPetPlayer() {
+
+ 
+// }
+
+
+// let selectpet = document.getElementById(`button-select-pet`)
+
+// selectpet.addEventListener(`click`, selectPetPlayer())
