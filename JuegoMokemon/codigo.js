125108ec38febebfebac4a82ef8e4f336213af8d
SaintHazzard <chamitos1998@hotmail.com>
2022-08-17 13:30:37 -0500

    Codigo optimizado, funciones crearmsj

--- JuegoMokemon/codigo.js
+++ JuegoMokemon/codigo.js
@@ -1,13 +1,7 @@
-// add value in index.html
-function addValue() {
- let inputCheck = document.querySelectorAll(`.select`);
- inputCheck.forEach((Element) => {
-  let addValue = Element.getAttribute(`id`);
-  Element.setAttribute(`value`, addValue.toLocaleLowerCase());
- });
-}
+
+///////////// Botones y elementos
 const mokemons = [
- ///////////// Botones y elementos
+ 
  { name: "Charizard", tipo: ["fuego", `fuego`] },
  { name: `Hipoge`, tipo: [`agua`, `agua`] },
  { name: `Geoude`, tipo: [`tierra`, `tierra`] },
@@ -15,63 +9,65 @@ const mokemons = [
  { name: `Tucapalma`, tipo: [`agua`, `tierra`] },
  { name: `Langostelvis`, tipo: [`agua`, `fuego`] },
 ];
+window.addEventListener(`load`, window);
+const inputCheck = document.querySelectorAll(`.select1`);
+const movsElements = [`agua`, `fuego`, `tierra`];
+const namePet = document.getElementById(`mascotajugador`);
+const namePetEnemy = document.getElementById(`mascotaenemigo`);
+const divMovs = document.getElementById(`select-mov`);
+const buttonFire = document.getElementById(`button-fire`);
+const buttonWater = document.getElementById(`button-water`);
+const buttonSand = document.getElementById(`button-sand`);
+const elegidoEnemie = enemyPet();
+const refresh = document.getElementById(`button-restart`);
+const panelMascotas = document.getElementById(`select-pet`);
+const selectPetPlayer = document.getElementById(`button-select-pet`);
+const containerImgAlly = document.getElementById(`imgAlly`);
+const containerImgEnemie = document.getElementById(`imgEnemie`);
 
-function startPlay() {
-  ///////////// Botones y elementos
-
-  const inputCheck = document.querySelectorAll(`.select1`);
-  const movsElements = [`agua`, `fuego`, `tierra`];
-  const namePet = document.getElementById(`mascotajugador`);
-  const namePetEnemy = document.getElementById(`mascotaenemigo`);
-  const divMovs = document.getElementById(`select-mov`);
-  const buttonFire = document.getElementById(`button-fire`);
-  const buttonWater = document.getElementById(`button-water`);
-  const buttonSand = document.getElementById(`button-sand`);
-  const elegidoEnemie = enemyPet();
-
-  const panelMascotas = document.getElementById(`select-pet`);
-  const selectPetPlayer = document.getElementById(`button-select-pet`);
-  const containerImgAlly = document.getElementById(`imgAlly`);
-  const containerImgEnemie = document.getElementById(`imgEnemie`);
-
-  function allyPet() {}
-
-  selectPetPlayer.addEventListener(`click`, () => {
-    inputCheck.forEach((element) => {
-      if (element.checked) {
-        let selectedMokemon = element.id;
-        mokemons.forEach((element) => {
-          if (selectedMokemon == element.name) {
-            let mokemonForFight = element;
-            panelMascotas.classList.toggle(`hidden`);
-            namePetEnemy.innerHTML = elegidoEnemie.name;
-            if (mokemonForFight) {
-              namePet.innerHTML = element.name;
-              divMovs.classList.toggle(`hidden`);
-              if (selectedMokemon) {
-                containerImgAlly.src = `${mokemonForFight.name}.png`;
-              }
-              if (elegidoEnemie) {
-                containerImgEnemie.src = `${elegidoEnemie.name}.png`;
-              }
-              element.tipo.forEach((value) => {
-                if (value == movsElements[0]) {
-                  buttonWater.classList.add(`button-moveon`);
-                  inputCheck.forEach((check) => check.classList.add(`hidden`));
-                } else if (value == movsElements[1]) {
-                  buttonFire.classList.add(`button-moveon`);
-                  inputCheck.forEach((check) => check.classList.add(`hidden`));
-                } else if (value == movsElements[2]) {
-                  buttonSand.classList.add(`button-moveon`);
-                  inputCheck.forEach((check) => check.classList.add(`hidden`));
-                }
-              });
+  ///////////// SELECION DE MASCOTA
+  ///////////// SELECION DE MASCOTA
+function startGame() {{
+  inputCheck.forEach((element) => {
+    if (element.checked) {
+      let selectedMokemon = element.id;
+      mokemons.forEach((element) => {
+        if (selectedMokemon == element.name) {
+          let mokemonForFight = element;
+          panelMascotas.classList.toggle(`hidden`);
+          namePetEnemy.innerHTML = elegidoEnemie.name;
+          if (mokemonForFight) {
+            namePet.innerHTML = element.name;
+            divMovs.classList.toggle(`hidden`);
+            if (selectedMokemon) {
+              containerImgAlly.src = `${mokemonForFight.name}.png`;
             }
+            if (elegidoEnemie) {
+              containerImgEnemie.src = `${elegidoEnemie.name}.png`;
+            }
+            element.tipo.forEach((value) => {
+              if (value == movsElements[0]) {
+                buttonWater.classList.add(`button-moveon`);
+                inputCheck.forEach((check) => check.classList.add(`hidden`));
+              } else if (value == movsElements[1]) {
+                buttonFire.classList.add(`button-moveon`);
+                inputCheck.forEach((check) => check.classList.add(`hidden`));
+              } else if (value == movsElements[2]) {
+                buttonSand.classList.add(`button-moveon`);
+                inputCheck.forEach((check) => check.classList.add(`hidden`));
+              }
+            });
           }
-        });
-      }
-    });
+        }
+      });
+    }
   });
+}
+ 
+}
+selectPetPlayer.addEventListener(`click`, startGame);
+///////////// SELECION DE MASCOTA
+///////////// SELECION DE MASCOTA
 
   let enemyLifes = 3;
   let userLifes = 3;
@@ -86,19 +82,21 @@ function startPlay() {
   function movStrong() {
     return Math.floor(Math.random() * (3 - 0 + 1) + 0);
   }
-  console.log(movStrong());
-  console.log(movWeaknes());
 
-  // BOTON ATAQUE AGUA
+
+ function mostrarRegistroCombate() {
+  document.getElementById(`menssageally`).classList.remove(`hidden`);
+  document.getElementById(`menssageenemie`).classList.remove(`hidden`);
+}
+ 
   // BOTON ATAQUE AGUA
   // BOTON ATAQUE AGUA
   buttonWater.addEventListener(`click`, () => {
-    const elegidoAlly = setAllyPet();
-    document.getElementById(`menssageally`).classList.remove(`hidden`);
-    document.getElementById(`menssageenemie`).classList.remove(`hidden`);
-
+   const elegidoAlly = setAllyPet();
+   mostrarRegistroCombate()
+ 
     let foundPlayer = mokemons.find((element) => element.name == elegidoAlly);
-let valueButton = 0;
+    let valueButton = 0;
     let ataqueFuerte = movStrong();
     let ataqueDebil = movWeaknes();
     let tipoAgua = foundPlayer.tipo.find((element) => element == movsElements[0]);
@@ -108,78 +106,49 @@ let valueButton = 0;
     if (tipoAgua && tipoTierra) {
       if (ataqueDebil > ataqueFuerte) {
         enemyLifes--;
-        crearMensajeAlly();
+        crearMensajeAlly(valueButton);
       } else {
         userLifes--;
-        crearMensajeEnemie();
+        crearMensajeEnemie(valueButton);
       }
       lifeEnemy.innerHTML = enemyLifes;
       lifeUser.innerHTML = userLifes;
     } else if (tipoAgua && tipoFuego) {
       if (ataqueFuerte >= ataqueDebil) {
         enemyLifes--;
-        crearMensajeAlly();
+        crearMensajeAlly(valueButton);
       } else {
         userLifes--;
-        crearMensajeEnemie();
+        crearMensajeEnemie(valueButton);
       }
       lifeEnemy.innerHTML = enemyLifes;
       lifeUser.innerHTML = userLifes;
     } else if (tipoAgua && tipoAgua) {
       if (movStrong() >= movStrong()) {
-        enemyLifes--;
+       enemyLifes--;
+       crearMensajeAlly(valueButton);
       } else {
-        userLifes--;
+       userLifes--;
+       crearMensajeEnemie(valueButton);
       }
       lifeEnemy.innerHTML = enemyLifes;
       lifeUser.innerHTML = userLifes;
-      crearMensajeAlly();
-      crearMensajeEnemie();
+      
+      
     }
 
-    function crearMensajeAlly() {
-      let parrafo = document.createElement(`p`);
-      let menssage = document.getElementById(`menssageally`);
-      parrafo.innerHTML = `Atacaste con ${movsElements[valueButton]} el enemigo con ${elegidoEnemie.tipo[movWeaknes()]
-      }`;
-      menssage.appendChild(parrafo);
-    }
-    function crearMensajeEnemie() {
-      let parrafo = document.createElement(`p`);
-      let menssage = document.getElementById(`menssageenemie`);
-      parrafo.innerHTML = `Atacaste con ${movsElements[valueButton]} el enemigo con ${
-        elegidoEnemie.tipo[movWeaknes()]
-      }`;
-      menssage.appendChild(parrafo);
-    }
 
-    const leftLifePLayer = setAllyLifes();
-    const leftLifeEnemies = setEnemieLifes();
-    if (leftLifeEnemies <= 0) {
-      document.getElementById(`aviso`).innerText = `GANASTE`;
-      document.getElementById(`button-restart`).classList.remove(`hidden`);
-      buttonFire.disabled = true;
-      buttonWater.disabled = true;
-      buttonSand.disabled = true;
-    }
-    if (leftLifePLayer <= 0) {
-      document.getElementById(`aviso`).innerText = `PERDISTE`;
-      document.getElementById(`button-restart`).classList.remove(`hidden`);
-      buttonFire.disabled = true;
-      buttonWater.disabled = true;
-      buttonSand.disabled = true;
-    }
+    endBattle();
   });
+ // BOTON ATAQUE AGUA
+  // BOTON ATAQUE AGUA
+  
 
   //// BOTON ATAQUE FUEGO
-
   //// BOTON ATAQUE FUEGO
-  //// BOTON ATAQUE FUEGO
-
   buttonFire.addEventListener(`click`, () => {
     const elegidoAlly = setAllyPet();
-    document.getElementById(`menssageally`).classList.remove(`hidden`);
-    document.getElementById(`menssageenemie`).classList.remove(`hidden`);
+    mostrarRegistroCombate()
    let valueButton = 1;
     let foundPlayer = mokemons.find((element) => element.name == elegidoAlly);
 
@@ -192,74 +161,46 @@ let valueButton = 0;
     if (tipoFuego && tipoAgua) {
       if (ataqueDebil > ataqueFuerte) {
         enemyLifes--;
-        crearMensajeAlly();
+        crearMensajeAlly(valueButton);
       } else {
         userLifes--;
-        crearMensajeEnemie();
+        crearMensajeEnemie(valueButton);
       }
       lifeEnemy.innerHTML = enemyLifes;
       lifeUser.innerHTML = userLifes;
     } else if (tipoFuego && tipoTierra) {
       if (ataqueFuerte >= ataqueDebil) {
         enemyLifes--;
-        crearMensajeAlly();
+        crearMensajeAlly(valueButton);
       } else {
         userLifes--;
-        crearMensajeEnemie();
+        crearMensajeEnemie(valueButton);
       }
       lifeEnemy.innerHTML = enemyLifes;
       lifeUser.innerHTML = userLifes;
     } else if (tipoFuego && tipoFuego) {
       if (movStrong() >= movStrong()) {
-        enemyLifes--;
+       enemyLifes--;
+       crearMensajeAlly(valueButton);
       } else {
-        userLifes--;
+       userLifes--;
+       crearMensajeEnemie(valueButton);
       }
       lifeEnemy.innerHTML = enemyLifes;
       lifeUser.innerHTML = userLifes;
-      crearMensajeAlly();
-      crearMensajeEnemie();
+      
+      
     }
-
-    function crearMensajeAlly() {
-      let parrafo = document.createElement(`p`);
-      let menssage = document.getElementById(`menssageally`);
-      parrafo.innerHTML = `Atacaste con ${movsElements[valueButton]} el enemigo con ${
-        elegidoEnemie.tipo[movWeaknes()]
-      }`;
-      menssage.appendChild(parrafo);
-    }
-    function crearMensajeEnemie() {
-      let parrafo = document.createElement(`p`);
-      let menssage = document.getElementById(`menssageenemie`);
-      parrafo.innerHTML = `Atacaste con ${movsElements[valueButton]} el enemigo con ${
-        elegidoEnemie.tipo[movWeaknes()]
-      }`;
-      menssage.appendChild(parrafo);
-    }
-
-    const leftLifePLayer = setAllyLifes();
-    const leftLifeEnemies = setEnemieLifes();
-    if (leftLifeEnemies <= 0) {
-      document.getElementById(`aviso`).innerText = `GANASTE`;
-      document.getElementById(`button-restart`).classList.remove(`hidden`);
-      buttonFire.disabled = true;
-      buttonWater.disabled = true;
-      buttonSand.disabled = true;
-    }
-    if (leftLifePLayer <= 0) {
-      document.getElementById(`aviso`).innerText = `PERDISTE`;
-      document.getElementById(`button-restart`).classList.remove(`hidden`);
-      buttonFire.disabled = true;
-      buttonWater.disabled = true;
-      buttonSand.disabled = true;
-    }
+    endBattle();
   });
- 
+  //// BOTON ATAQUE FUEGO
+  //// BOTON ATAQUE FUEGO
+
+////// BOTON ATAQUE ARENA
+////// BOTON ATAQUE ARENA
  buttonSand.addEventListener(`click`, () => {
    const elegidoAlly = setAllyPet();
-   document.getElementById(`menssageally`).classList.remove(`hidden`);
-  document.getElementById(`menssageenemie`).classList.remove(`hidden`);
+   mostrarRegistroCombate()
   let valueButton = 2;
 
    let foundPlayer = mokemons.find((element) => element.name == elegidoAlly);
@@ -273,81 +214,81 @@ let valueButton = 0;
    if (tipoTierra && tipoFuego) {
      if (ataqueDebil > ataqueFuerte) {
        enemyLifes--;
-       crearMensajeAlly();
+       crearMensajeAlly(valueButton);
      } else {
        userLifes--;
-       crearMensajeEnemie();
+       crearMensajeEnemie(valueButton);
      }
      lifeEnemy.innerHTML = enemyLifes;
      lifeUser.innerHTML = userLifes;
    } else if (tipoTierra && tipoAgua) {
      if (ataqueFuerte >= ataqueDebil) {
        enemyLifes--;
-       crearMensajeAlly();
+       crearMensajeAlly(valueButton);
      } else {
        userLifes--;
-       crearMensajeEnemie();
+       crearMensajeEnemie(valueButton);
      }
      lifeEnemy.innerHTML = enemyLifes;
      lifeUser.innerHTML = userLifes;
    } else if (tipoTierra && tipoTierra) {
      if (movStrong() >= movStrong()) {
-       enemyLifes--;
+      enemyLifes--;
+      crearMensajeAlly(valueButton);
      } else {
-       userLifes--;
+      userLifes--;
+      crearMensajeEnemie(valueButton);
      }
      lifeEnemy.innerHTML = enemyLifes;
      lifeUser.innerHTML = userLifes;
-     crearMensajeAlly();
-     crearMensajeEnemie();
+     
+     
    }
-
-   function crearMensajeAlly() {
-     let parrafo = document.createElement(`p`);
-     let menssage = document.getElementById(`menssageally`);
-     parrafo.innerHTML = `Atacaste con ${movsElements[valueButton]} el enemigo con ${
-       elegidoEnemie.tipo[movWeaknes()]
-     }`;
-     menssage.appendChild(parrafo);
-   }
-   function crearMensajeEnemie() {
-     let parrafo = document.createElement(`p`);
-     let menssage = document.getElementById(`menssageenemie`);
-     parrafo.innerHTML = `Atacaste con ${movsElements[valueButton]} el enemigo con ${
-       elegidoEnemie.tipo[movWeaknes()]
-     }`;
-     menssage.appendChild(parrafo);
-   }
-
-   const leftLifePLayer = setAllyLifes();
-   const leftLifeEnemies = setEnemieLifes();
-   if (leftLifeEnemies <= 0) {
-     document.getElementById(`aviso`).innerText = `GANASTE`;
-     document.getElementById(`button-restart`).classList.remove(`hidden`);
-     buttonFire.disabled = true;
-     buttonWater.disabled = true;
-     buttonSand.disabled = true;
-   }
-   if (leftLifePLayer <= 0) {
-     document.getElementById(`aviso`).innerText = `PERDISTE`;
-     document.getElementById(`button-restart`).classList.remove(`hidden`);
-     buttonFire.disabled = true;
-     buttonWater.disabled = true;
-     buttonSand.disabled = true;
-   }
+   endBattle();
+   
  });
+////// BOTON ATAQUE ARENA
+////// BOTON ATAQUE ARENA
+
+function crearMensajeAlly(valueButton) {
+  let parrafo = document.createElement(`p`);
+  let menssage = document.getElementById(`menssageally`);
+  parrafo.innerHTML = `Atacaste con ${movsElements[valueButton]} el enemigo con ${
+    elegidoEnemie.tipo[movWeaknes()]
+  }`;
+  menssage.appendChild(parrafo);
+}
+function crearMensajeEnemie(valueButton) {
+  let parrafo = document.createElement(`p`);
+  let menssage = document.getElementById(`menssageenemie`);
+  parrafo.innerHTML = `Atacaste con ${movsElements[valueButton]} el enemigo con ${
+    elegidoEnemie.tipo[movWeaknes()]
+  }`;
+  menssage.appendChild(parrafo);
 }
 
-////// BOTON ATAQUE ARENA
-////// BOTON ATAQUE ARENA
-////// BOTON ATAQUE ARENA
-////// BOTON ATAQUE ARENA
-////// BOTON ATAQUE ARENA
-////// BOTON ATAQUE ARENA
+function endBattle() {
+ const leftLifePLayer = setAllyLifes();
+ const leftLifeEnemies = setEnemieLifes();
+ console.log(leftLifeEnemies);
+ if (leftLifeEnemies <= 0) {
+   document.getElementById(`aviso`).innerText = `GANASTE`;
+   document.getElementById(`button-restart`).classList.remove(`hidden`);
+   buttonFire.disabled = true;
+   buttonWater.disabled = true;
+   buttonSand.disabled = true;
+ }
+ if (leftLifePLayer <= 0) {
+   document.getElementById(`aviso`).innerText = `PERDISTE`;
+   document.getElementById(`button-restart`).classList.remove(`hidden`);
+   buttonFire.disabled = true;
+   buttonWater.disabled = true;
+   buttonSand.disabled = true;
+ }
+}
 
 // funcion para elegir mascota enemiga de manera aleatoria
 function enemyPet() {
- let inputCheck = document.querySelectorAll(`.select`);
  let randomPet = Math.floor(Math.random() * (5 - 0 + 1) + 0);
 
  let eledido = mokemons[randomPet];
@@ -369,12 +310,19 @@ function setEnemieLifes() {
  return test;
 }
 
-
-
-window.addEventListener(`load`, startPlay);
+window.addEventListener(`load`, window);
 
 function restart() {
- let refresh = document.getElementById(`button-restart`);
+ 
  location.reload();
+}
+
+// add value in index.html
+function addValue() {
+ let inputCheck = document.querySelectorAll(`.select1`);
+ inputCheck.forEach((Element) => {
+  let addValue = Element.getAttribute(`id`);
+  Element.setAttribute(`value`, addValue);
+ });
 }
 
